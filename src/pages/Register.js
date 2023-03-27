ed283a83d2e7555191c8cf7b033c01906910c0f8
kyungchan Choi <kyungchanchoi@kyungchanui-MacBookAir.local>
2023-03-27 19:35:35 +0900

    feat: Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,10 +2,32 @@ import React from "react";
 
 export default function Register() {
   
-  
+  const handleClickRegister = (e) => {
+    e.preventDefault();
+  }
+
   return(
     <form>
-      
+      <div display="flex" align="center" text-align="right"> {/* float로 위치 오른쪽 왼쪽 조절가능 */}
+        <h1>Sign Up</h1>
+        <div postion="relative">
+          <label for="floatingInput">Email address</label>
+          <input type="email" placeholder="name@address.com" />        
+        </div>
+        <div postion="relative">
+          <label for="floatingInput" postion="absolute">Password</label>
+          <input type="password" placeholder="****" />
+        </div>
+        <div postion="relative">
+          <label for="floatingInput" postion="absolute">Check-Password</label>
+          <input type="password" placeholder="****" />
+        </div>
+        <div postion="relative">
+          <label for="floatingInput">Name</label>
+          <input type="email" placeholder="Rachel" />        
+        </div>
+        <button type="submit" onClick={handleClickRegister}>Sign Up</button>
+      </div>
     </form>
   );
 }
