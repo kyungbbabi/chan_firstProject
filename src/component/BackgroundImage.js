bd8787ba6bbe4f79e92046aeef2befcdfebbe6f0
kyungchan <kyung981124@naver.com>
2023-04-03 15:13:48 +0900

    typing 2

--- src/component/BackgroundImage.js
+++ src/component/BackgroundImage.js
@@ -8,3 +8,32 @@ export default function BackgroundImage(props) {
     </div>
   );
 }
+
+
+
+
+// 이미지 직접쓸 때
+import img1 from "A.png"
+import img2 from "B.png"
+import img3 from "C.png"
+import img4 from "D.png"
+import img5 from "E.png"
+import img6 from "F.png"
+
+function sub(props){
+  const backgroundImgArr = [img1, img2, img3, img4, img5, img6];
+  const randomIndex = Math.floor(Math.random() * backgroundImgArr.length);
+
+  const backgroundImg = backgroundImgArr[randomIndex];
+  
+  return (
+    <section style={{backgroundImage: `url(${backgroundImg})`}}>
+      
+      <p> Hello, {userID} </p>
+
+        <p>What is your main focus for today?</p>
+        <input />
+
+    </section>
+  )
+}
