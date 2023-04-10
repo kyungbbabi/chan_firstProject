4d0163074bff73ed1ee30535f4476caa393e5ec8
kyungbbabi <kyung981124@naver.com>
2023-04-10 20:19:48 +0900

    Homework 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LoginImg from "../test.png"
 
 export default function Login(props) {
 
@@ -8,28 +9,25 @@ export default function Login(props) {
   
   return (
     <>
-      <section style={{position:"relative", justifyContent:"center", alignItem:"center"}}>
+      <section style={{position:"relative", display:"flex", justifyContent:"center", alignItem:"center", height:"500px"}}>
         <div style={{position:"absolute", display:"flex", flexDirection:"row"}}>  
           <div>
-            <div>
-              hi
-            </div>
+            <img src={LoginImg} style={{maxWidth:"500px", maxHeight:"500px"}}/>
           </div>
-          <div style={{display:"grid", textAlign:"center", backgroundColor:"white"}}> {/* float로 위치 오른쪽 왼쪽 조절가능 */}
-            <h1>Sign In</h1>
+          <div style={{display:"grid", justifyContent:"center", alignItems:"center", textAlign:"center", marginBottom:"20px", backgroundColor:"white", width:"350px", height:"500px"}}> {/* float로 위치 오른쪽 왼쪽 조절가능 */}
+            <h1 style={{fontSize:"xxx-large"}}>Sign In</h1>
             <div>
               <input type="email" placeholder="name@address.com" />
             </div>
-            <div postion="relative">
+            <div>
               <input type="password" placeholder="****" />              
             </div>
-            <div postion="relative" margin-bottom="1rem">
+            <div>
               <label><input type="checkbox" />Remember me</label>
             </div>
-            <button type="submit" onClick={handleClickLogin}>Sign In</button>
+            <button type="submit" style={{height:"25%" ,borderRadius:"5%", display:"flex", justifyContent:"center"}} onClick={handleClickLogin}>Sign In</button>
           </div>
-        
-        </div>
+        </div>  
       </section>
     </>
   );
