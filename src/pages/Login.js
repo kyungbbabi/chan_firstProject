421322b20e287a45b33abaa176978c25ce90e040
kyungbbabi <kyung981124@naver.com>
2023-04-11 11:09:45 +0900

    homework 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,22 +10,22 @@ export default function Login(props) {
   return (
     <>
       <section style={{position:"relative", display:"flex", justifyContent:"center", alignItem:"center", height:"500px"}}>
-        <div style={{position:"absolute", display:"flex", flexDirection:"row"}}>  
+        <div style={{position:"absolute", display:"flex", flexDirection:"row", maxHeight:"500px"}}>  
           <div>
             <img src={LoginImg} style={{maxWidth:"500px", maxHeight:"500px"}}/>
           </div>
-          <div style={{display:"grid", justifyContent:"center", alignItems:"center", textAlign:"center", marginBottom:"20px", backgroundColor:"white", width:"350px", height:"500px"}}> {/* float로 위치 오른쪽 왼쪽 조절가능 */}
+          <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", marginBottom:"20px", backgroundColor:"white", width:"350px", height:"500px"}}>
             <h1 style={{fontSize:"xxx-large"}}>Sign In</h1>
             <div>
               <input type="email" placeholder="name@address.com" />
             </div>
-            <div>
+            <div style={{marginBottom:"5%"}}>
               <input type="password" placeholder="****" />              
             </div>
-            <div>
+            <div style={{marginBottom:"5%"}}>
               <label><input type="checkbox" />Remember me</label>
             </div>
-            <button type="submit" style={{height:"25%" ,borderRadius:"5%", display:"flex", justifyContent:"center"}} onClick={handleClickLogin}>Sign In</button>
+            <button type="submit" style={{width:"40%" ,borderRadius:"5%", display:"flex", justifyContent:"center"}} onClick={handleClickLogin}>Sign In</button>
           </div>
         </div>  
       </section>
