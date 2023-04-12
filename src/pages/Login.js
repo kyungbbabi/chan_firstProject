ee9314da9db716be1f0a781a8cd906ea432d1237
kyungbbabi <kyung981124@naver.com>
2023-04-12 17:15:37 +0900

    homework 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginImg from "../test.png"
 
 export default function Login(props) {
-
-  const handleClickLogin = (e) => {
-    e.preventDefault();
-  }
   
+  const inputStyles = {
+    width: "100%",
+    height: "35px",
+    padding: "5px",
+    marginBottom: "10px",
+    fontSize: "1.2rem",
+    border: "1px solid #ddd",
+    borderRadius: "3px",
+  };
+  const labelStyles = {
+    position: "absolute",
+    top: "0",
+    left: "5px",
+    color: "#999",
+    fontSize: "1rem",
+    transition: "all 0.2s ease-out",
+    transformOrigin: "0 0",
+    pointerEvents: "none",
+  };
+
+    // state variables to keep track of input field values
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    // event handlers to update state variables
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+    };
+
+    const handleClickLogin = (e) => {
+      e.preventDefault();
+    }
+
+
   return (
     <>
       <section style={{position:"relative", display:"flex", justifyContent:"center", alignItem:"center", height:"500px"}}>
@@ -14,15 +47,17 @@ export default function Login(props) {
           <div>
             <img src={LoginImg} style={{maxWidth:"500px", maxHeight:"500px"}}/>
           </div>
-          <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", marginBottom:"20px", backgroundColor:"white", width:"350px", height:"500px"}}>
+          <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", backgroundColor:"white", width:"350px", height:"500px"}}>
             <h1 style={{fontSize:"xxx-large"}}>Sign In</h1>
-            <div>
-              <input type="email" placeholder="name@address.com" />
+            <div style={{position:"relative"}}>
+              <input type="email" style={inputStyles}  value={email} onChange={handleEmailChange}/>
+              <label style={{...labelStyles, opacity: email ? "0" : "1", transform: email ? "translateY(-1.5rem)" : "none",}}>Email address</label>
             </div>
-            <div style={{marginBottom:"5%"}}>
-              <input type="password" placeholder="****" />              
+            <div style={{position:"relative", marginBottom:"20px"}}>
+              <input type="password" style={inputStyles} placeholder="****" value={password} onChange={handlePasswordChange}/>              
+              <label style={{...labelStyles, opacity: password ? "0" : "1", transform: password ? "translateY(-1.5rem)" : "none",}}>Password</label>
             </div>
-            <div style={{marginBottom:"5%"}}>
+            <div style={{marginBottom:"20px"}}>
               <label><input type="checkbox" />Remember me</label>
             </div>
             <button type="submit" style={{width:"40%" ,borderRadius:"5%", display:"flex", justifyContent:"center"}} onClick={handleClickLogin}>Sign In</button>
