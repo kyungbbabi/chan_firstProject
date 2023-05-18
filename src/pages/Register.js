1e73ae3d096a34f4f08d16fed640cf2f30a10150
kyungbbabi <kyung981124@naver.com>
2023-05-18 19:22:50 +0900

    docs: register page 1

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Stack, Typography, Box, TextField, FormControl, InputLabel, Input, InputAdornment, IconButton, MenuItem } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-export default function Register() {
-  
+export default function Register(props) {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  };
+
   const handleClickRegister = (e) => {
     e.preventDefault();
   }
 
+  const genders = [ {value:'Man', label:'Man'}, {value:"Woman", label:"Woman"}]
+
   return(
-    <form>
-      <div display="flex" align="center" text-align="right"> {/* float로 위치 오른쪽 왼쪽 조절가능 */}
-        <h1>Sign Up</h1>
-        <div postion="relative">
-          <label for="floatingInput">Email address</label>
-          <input type="email" placeholder="name@address.com" />        
-        </div>
-        <div postion="relative">
-          <label for="floatingInput" postion="absolute">Password</label>
-          <input type="password" placeholder="****" />   
-        </div>
-        <div postion="relative">
-          <label for="floatingInput" postion="absolute">Check-Password</label>
-          <input type="password" placeholder="****" />
-        </div>
-        <div postion="relative">
-          <label for="floatingInput">Name</label>
-          <input type="email" placeholder="Rachel" />        
-        </div>
-        <button type="submit" onClick={handleClickRegister}>Sign Up</button>
-      </div>
-    </form>
+    <Stack spacing={2}>
+      <Typography variant="h5">Sign Up</Typography>
+      
+        <TextField variant="standard" label="email address">Email Address</TextField>
+        <TextField variant="standard" type="password" label="password">Password</TextField>
+        <FormControl variant="standard">
+          <InputLabel>confirm</InputLabel>
+          <Input type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="confirm"
+          />
+        </FormControl>
+        <TextField variant="standard" label="name">Name</TextField>
+        <TextField variant="standard" label="recovery email address">Recovery Email Address</TextField>
+        <TextField variant="standard" label="Phone number">Phone Number</TextField>
+        <TextField variant="standard" label="gender">
+          
+          Gender
+
+          {genders.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+          </TextField>
+      
+    </Stack>
   );
 }
