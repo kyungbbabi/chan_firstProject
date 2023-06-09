12b3223ad5e8035b920eb9de7e62ddd1a98389cf
kyungbbabi <kyung981124@naver.com>
2023-06-09 18:26:17 +0900

    docs: Header Component fix 1

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
 import HeaderMenu from "./HeaderMenu";
-import { AppBar } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import HeaderAvatar from "./HeaderAvatar";
 
 const Header = (props) => {
   
 
   return(
     <>
-      <AppBar>
-        <HeaderMenu />
+      <AppBar >
+        <Toolbar style={{padding:"0", display:"flex", justifyContent:"space-between"}}>
+          <HeaderMenu />
+
+          {/* 위치 맞춰봐야함 */}
+          <Typography variant="h6" align="center">Portfolio</Typography> 
+          
+          <HeaderAvatar />
+        
+        </Toolbar>
       </AppBar>
     </>
   );
--- src/component/header/HeaderMenu.js
+++ src/component/header/HeaderMenu.js
@@ -1,24 +1,26 @@
-import { Tooltip, MenuItem } from "@mui/material";
+import { Tooltip, MenuItem, AppBar, Toolbar } from "@mui/material";
 import React from "react";
 
 const HeaderMenu = (props) => {
   
   
   return(
-    <>
-      <Tooltip title="Home" placement="left" arrow>
-        <MenuItem>Home</MenuItem>
-      </Tooltip>
-      <Tooltip title="Blog">
-        <MenuItem>Blog</MenuItem>
-      </Tooltip>
-      <Tooltip title="Portfolio">
-        <MenuItem>Portfolio</MenuItem>
-      </Tooltip>
-      <Tooltip title="Main">
-        <MenuItem>Main</MenuItem>
-      </Tooltip>
-    </>
+    
+      <Toolbar style={{padding:"0"}}>
+        <Tooltip title="Home">
+          <MenuItem>Home</MenuItem> 
+        </Tooltip>
+        <Tooltip title="Blog">
+          <MenuItem>Blog</MenuItem>
+        </Tooltip>
+        <Tooltip title="Portfolio">
+          <MenuItem>Portfolio</MenuItem>
+        </Tooltip>
+        <Tooltip title="Main">
+          <MenuItem>Main</MenuItem>
+        </Tooltip>
+      </Toolbar>
+   
   );
 }
 
