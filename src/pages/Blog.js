b327641cb5977e6b88e6638dca0f91d9ae70e657
kyungbbabi <kyung981124@naver.com>
2023-09-12 22:47:27 +0900

    docs: Hard Coding 1

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,9 +1,56 @@
+import { Box, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Grid, IconButton, Typography } from "@mui/material";
 import React from "react";
+import Header from "../component/header/Header";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Newyork from '../newyork.png'
 
 export default function Blog(){
 
   return(
-    <></>
+    <Box>
+      <Header />
+      <Grid sx={{display: "flex", alignItems:"center", justifyContent:"center", marginTop:"4em", padding:"3em"}} >
+        <Card sx={{maxWidth: 345}}>
+          <CardMedia component="img" height="194" image={Newyork} />
+          <CardContent>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Typography>
+              Title 1, yyyy mm dd
+            </Typography>
+            <IconButton>
+              <FavoriteIcon fontSize="small"/>
+            </IconButton>
+          </CardActions>
+        </Card>
+        <Card sx={{maxWidth: 345}}>
+          <CardHeader action={<IconButton><MoreVertIcon /> </IconButton> } title="title 2" subheader="date"/>
+          <CardMedia component="img" height="194" image={Newyork} alt="Paella dish" />
+          <CardContent>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{maxWidth: 345}}>
+          <CardHeader action={<IconButton><MoreVertIcon /> </IconButton> } title="title 3" subheader="date"/>
+          <CardMedia component="img" height="194" image={Newyork} alt="Paella dish" />
+          <CardContent>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Box>
   );
 
 }
