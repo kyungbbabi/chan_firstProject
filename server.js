0d06fe499c7dfd4025a52b3942e05ed26cc44734
kyungbbabi <kyung981124@naver.com>
2023-10-12 18:20:16 +0900

    test: firebase add 2

--- server.js
+++ server.js
@@ -2,9 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser'); //server module
 const app = express();
 const port = process.env.PORT || 5000;
+const admin = require('firebase-admin');
+const serviceAccount = require('path/to/serviceAccountKey.json'); // Firebase 서비스 계정 키 경로
 
 
 // JSON 파싱 미들웨어 설정
+// // 미들웨어 함수 정의
+// app.use((req, res, next) => {
+//   console.log('미들웨어 1: 요청이 들어옴');
+//   next(); // 다음 미들웨어 또는 라우트 핸들러로 요청 전달
+// });
+
+// app.use((req, res, next) => {
+//   console.log('미들웨어 2: 요청을 처리함');
+//   // 응답 보내지 않고 다음 미들웨어로 전달
+//   next();
+// });
+
+// // 라우트 핸들러
+// app.get('/', (req, res) => {
+//   console.log('라우트 핸들러: 루트 경로 요청 처리');
+//   res.send('안녕, Express!');
+// });
+
+// // 서버 시작
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,6 +43,62 @@ app.get('/api/Login', (req, res) => {
 app.listen(port, () => {
 
 });
+
+// // 미들웨어 설정
+// app.use(express.json());
+
+// // 라우트 설정
+// app.post('/login', (req, res) => {
+//   // 로그인 로직을 구현합니다. Firebase와 연동하여 사용자 인증을 처리할 수 있습니다.
+//   const { email, password } = req.body;
+//   // 사용자 인증 및 토큰 생성 로직을 여기에 추가합니다.
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://your-firebase-project.firebaseio.com',
+// });
+
+// // Firebase 인증 설정
+// const auth = admin.auth();
+
+// // Firebase 데이터베이스 설정
+// const db = admin.firestore();
+
+// // 사용자 로그인 및 Firebase 사용자 생성
+// app.post('/login', async (req, res) => {
+//   const { email, password } = req.body;
+
+//   // Firebase 사용자 인증
+//   try {
+//     const user = await auth.getUserByEmail(email);
+//     // 사용자가 존재하면 로그인 로직 수행
+//     // 이메일 및 비밀번호를 사용하여 사용자 인증을 수행할 수 있습니다.
+//   } catch (error) {
+//     // 사용자가 존재하지 않음. 회원가입 또는 오류 처리를 수행합니다.
+//   }
+// });
+
+// // Firebase 데이터베이스 사용 예시
+// app.get('/data', async (req, res) => {
+//   // Firebase 데이터베이스에서 데이터를 읽거나 쓸 수 있습니다.
+//   const dataRef = db.collection('dataCollection').doc('documentId');
+//   const snapshot = await dataRef.get();
+//   if (snapshot.exists) {
+//     const data = snapshot.data();
+//     res.json(data);
+//   } else {
+//     res.status(404).json({ message: 'Data not found' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 
 
