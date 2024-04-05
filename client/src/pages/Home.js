fd4681718e2e7a607146db28fa6e3c02758110ab
kyungbbabi <kyung981124@naver.com>
2024-04-05 18:16:29 +0900

    add home.js unsplash api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,9 +26,7 @@ export default function Home(props) {
   useEffect(() => {
     const getImages = async () => {
       try {
-        const response = await axios.get('https://api.unsplash.com/photos/random', {
-          params: { client_id: process.env.REACT_APP_UNSPLASH_ACCESS_KEY, count: 30 }
-        })
+        const response = await axios.get(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`)
         setImages(response.data);
       } catch (error) {
         console.error('Error fetching images:', error);
@@ -50,43 +48,37 @@ export default function Home(props) {
   };
 
   return (
-
-    <>
-      <Box>
-        {images}
-      </Box>
-    </>
-    // <Box>
-    //   {images.length > 0 && (
-    //     <>
-    //       <Paper square elevation={0} sx={{ display: 'flex', alignItems: 'center', height: 50, pl: 2, bgcolor: 'background.default', }} >
-    //         <Typography>{images[activeStep].label}</Typography>
-    //       </Paper>
-    //       <AutoPlaySwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents >
-    //         {images.map((step, index) => (
-    //           <div key={step.label}>
-    //             {Math.abs(activeStep - index) <= 2 ? (
-    //               <Box component="img" sx={{ height: 255, display: 'block', maxWidth: 400, overflow: 'hidden', width: '100%', }} src={step.imgPath} alt={step.label} />
-    //             ) : null}
-    //           </div>
-    //         ))}
-    //       </AutoPlaySwipeableViews>
-    //       <MobileStepper steps={maxSteps} position="static" activeStep={activeStep}
-    //         nextButton={
-    //           <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1} >
-    //             Next
-    //             {theme.direction === 'rtl' ? (<KeyboardArrowLeft />) : (<KeyboardArrowRight />)}
-    //           </Button>
-    //         }
-    //         backButton={
-    //           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-    //             {theme.direction === 'rtl' ? (<KeyboardArrowRight />) : (<KeyboardArrowLeft />)}
-    //             Back
-    //           </Button>
-    //         }
-    //       />
-    //     </>
-    //   )}
-    // </Box>
+    <Box>
+      {images.length > 0 && (
+        <>
+          <Paper square elevation={0} sx={{ display: 'flex', alignItems: 'center', height: 50, pl: 2, bgcolor: 'background.default', }} >
+            <Typography>{images[activeStep].label}</Typography>
+          </Paper>
+          <AutoPlaySwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents >
+            {images.map((step, index) => (
+              <div key={step.label}>
+                {Math.abs(activeStep - index) <= 2 ? (
+                  <Box component="img" sx={{ height: 255, display: 'block', maxWidth: 400, overflow: 'hidden', width: '100%', }} src={step.urls.regular} alt={step.alt_description} />
+                ) : null}
+              </div>
+            ))}
+          </AutoPlaySwipeableViews>
+          <MobileStepper steps={maxSteps} position="static" activeStep={activeStep}
+            nextButton={
+              <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1} >
+                Next
+                {theme.direction === 'rtl' ? (<KeyboardArrowLeft />) : (<KeyboardArrowRight />)}
+              </Button>
+            }
+            backButton={
+              <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                {theme.direction === 'rtl' ? (<KeyboardArrowRight />) : (<KeyboardArrowLeft />)}
+                Back
+              </Button>
+            }
+          />
+        </>
+      )}
+    </Box>
   );
 }
