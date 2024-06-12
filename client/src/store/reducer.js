920961faeedeca90931351935c6956bc9637ea90
kyungbbabi <kyung981124@naver.com>
2024-06-12 20:29:55 +0900

    feat: loginpage detail 2

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -2,6 +2,10 @@ export default function reducer(state, action) {
   switch (action.type) {
     case 'User':
       return { ...state, user: action.payload };
+    case 'OpenLoading':
+      return {...state, loading: {open: true, message: action.payload}};
+    case 'CloseLoading':
+      return {...state, loading: {open: false, message: ''}};
     default:
       throw Error(`${action.type}: 알 수 없는 타입입니다.`);
   }
