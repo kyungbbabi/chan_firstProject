f1dad9e12d784bba6d2f29b489eae9fa7c4262e1
kyungbbabi <kyung981124@naver.com>
2024-06-20 20:11:34 +0900

    feat: 헤더에 글쓰기 버튼 추가

--- client/src/component/header/Header.js
+++ client/src/component/header/Header.js
@@ -1,28 +1,45 @@
 import React from "react";
 import HeaderMenu from "./HeaderMenu";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, IconButton } from "@mui/material";
 import HeaderAvatar from "./HeaderAvatar";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useLocation } from "react-router-dom";
+import PostAddIcon from '@mui/icons-material/PostAdd';
 
 const Header = (props) => {
   
   let navigate = useNavigate();
+  let location = useLocation();
 
   const handleClickHome = () => {
     navigate('/');
   };
 
+  const handelClickPostWrite = () => {
+    navigate('/postwrite');
+  };
+
+  const showPostWriteIcon = location.pathname === '/blog' || location.pathname === '/portfolio';
+
   //left:"50%", transform:"translateX(-50%)" 없이 Lorem ipsum dolor가 정 가운데로 오는 방법
   return(
     <Box marginBottom="3%">
-      <AppBar>
-        <Toolbar style={{ padding: "0", display: "flex", justifyContent: "space-between" }}>
-          <HeaderMenu />
-          <Typography variant="h6" style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }} onClick={handleClickHome}>Lorem ipsum dolor</Typography> 
+    <AppBar>
+      <Toolbar style={{ padding: "0", display: "flex", justifyContent: "space-between" }}>
+        <HeaderMenu />
+        <Typography variant="h6" style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }} onClick={handleClickHome}>
+          Lorem ipsum dolor
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {showPostWriteIcon && (
+            <IconButton color="inherit" onClick={handelClickPostWrite} sx={{ marginRight: 2 }}>
+              <PostAddIcon />
+            </IconButton>
+          )}
           <HeaderAvatar />
-        </Toolbar>
-      </AppBar>
-    </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  </Box>
   );
 
 }
