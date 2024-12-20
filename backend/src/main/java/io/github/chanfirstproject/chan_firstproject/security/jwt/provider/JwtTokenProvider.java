8977523e04614e1e1c116fb7f97dfe0f1f5edad7
kyungbbabi <kyung981124@naver.com>
2024-12-20 19:36:44 +0900

    chore: add application-local.yml and edit gitignore

--- backend/src/main/java/io/github/chanfirstproject/chan_firstproject/security/jwt/provider/JwtTokenProvider.java
+++ backend/src/main/java/io/github/chanfirstproject/chan_firstproject/security/jwt/provider/JwtTokenProvider.java
@@ -1,7 +1,7 @@
 package io.github.chanfirstproject.chan_firstproject.security.jwt.provider;
 
+  // 실제 JWT 토큰을 생성 / 토큰 유효성 검증 / 토큰에서 사용자 정보 추출
+
 public class JwtTokenProvider {
-  // 실제 JWT 토큰을 생성
-  // 토큰 유효성 검증
-  // 토큰에서 사용자 정보 추출
+
 }
