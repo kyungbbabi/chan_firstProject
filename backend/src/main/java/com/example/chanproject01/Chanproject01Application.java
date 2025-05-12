8a781a254d73d428a1b15c5a285e337a2fc12b2e
kyungbbabi <kyung981124@naver.com>
2025-05-12 18:46:34 +0900

    Add new Spring Boot backend project

---
+++ backend/src/main/java/com/example/chanproject01/Chanproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.chanproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chanproject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chanproject01Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/chanproject01/Chanproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chanproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chanproject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
