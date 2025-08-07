89a13bd68a30d88b7b17f3325c2966f641860521
kyungbbabi <kyung981124@naver.com>
2025-08-07 18:54:30 +0900

    docs: blog api 설정

--- frontend/src/api/user/blog.js
+++ frontend/src/api/user/blog.js
@@ -1,9 +1,11 @@
 import api from "../index";
 
 export const blogApi = {
-  getPosts: (params) => api.get('/posts', params),
-  getPost: (id) => api.get(`/posts/${id}`),
-  createPost: (data) => api.post('/posts', data),
-  updatePost: (id, data) => api.put(`/posts/${id}`, data),
-  deletePost: (id) => api.delete(`/posts/${id}`)
+
+  getPosts: (params) => api.get('/api/blog', { params }),
+  getPost: (id) => api.get(`/api/blog/${id}`),
+  createPost: (data) => api.post('/api/blog', data),
+  updatePost: (id, data) => api.put(`/api/blog/${id}`, data),
+  deletePost: (id) => api.delete(`/api/blog/${id}`)
+
 };
