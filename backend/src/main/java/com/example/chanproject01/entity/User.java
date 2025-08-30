c5f3fcf087e0ca3b8a632c4e8ca53d958205f1d9
kyungbbabi <kyung981124@naver.com>
2025-08-30 16:51:24 +0900

    feat: user entity추가

---
+++ backend/src/main/java/com/example/chanproject01/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.chanproject01.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
