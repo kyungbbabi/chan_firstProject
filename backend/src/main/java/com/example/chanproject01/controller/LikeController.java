ed04c9874ee1dba9da465dc824f296b9c1ae06c7
kyungbbabi <kyung981124@naver.com>
2025-09-06 20:35:43 +0900

    docs: LikeController 추가

--- backend/src/main/java/com/example/chanproject01/controller/LikeController.java
+++ backend/src/main/java/com/example/chanproject01/controller/LikeController.java
@@ -1,7 +1,63 @@
 package com.example.chanproject01.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.chanproject01.dto.like.LikeRequestDto;
+import com.example.chanproject01.dto.like.LikeResponseDto;
+import com.example.chanproject01.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/likes")
+@RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/toggle")
+    public ResponseEntity<LikeResponseDto> toggleLike(@RequestBody LikeRequestDto likeRequestDto, @RequestParam Long userId) {
+        try {
+            LikeResponseDto response = likeService.toggleLike(likeRequestDto, userId);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<LikeResponseDto>> getUserLikesList(@RequestParam String contentType ,@RequestParam Long userId) {
+        try {
+            List<LikeResponseDto> likeUsers = likeService.getLikeUsersList(contentType, userId);
+            return ResponseEntity.ok(likeUsers);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<LikeResponseDto> getCurrentUserLikesStatus(@RequestParam String contentType, @RequestParam Long contentId, @RequestParam Long userId) {
+        try {
+            LikeResponseDto response = likeService.getCurrentUserLikesStatus(contentType, contentId, userId);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Map<String, Long>> getLikeCount(@RequestParam String contentType, @RequestParam Long contentId) {
+        try {
+            LikeResponseDto response = likeService.getCurrentUserLikesStatus(contentType, contentId, null);
+            Map<String, Long> result = new HashMap<>();
+            result.put("count", response.getLikeCount());
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
